ed787502abb7839367c3a08e61fb2792e11b7550
kagemusha <mmadrid99@gmail.com>
2013-02-11 16:38:47 -0500

    added ember user model and user serializer

---
+++ app/assets/javascripts/models/user.js.coffee
@@ -0,0 +1,3 @@
+App.User  = DS.Model.extend
+  name:   DS.attr('string')
+  email:  DS.attr('string')
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,4 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :email, :name
+
+end
