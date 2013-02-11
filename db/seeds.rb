687a4bbbfa9a85d70458e1fb23c01ddc9e4c9117
kagemusha <mmadrid99@gmail.com>
2013-02-11 17:00:48 -0500

    added user to db/seeds.rb and changed devise min password length to 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! :name => "TestUser", :email => "test@test.com", :password => "tester", :password_confirmation => "tester"
