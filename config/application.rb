87f24c7899e3f33dff6e43d143f665c74cdf1593
kagemusha <mmadrid99@gmail.com>
2013-02-28 00:13:20 -0500

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -75,6 +75,8 @@ module EmberRailsDevise
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    #for Heroku
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
